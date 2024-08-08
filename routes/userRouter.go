6b3aee072fd16e5e313ce9b42cf9791842026609
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-08 13:11:45 +0100

    Feat: user router method set up to reroute to controller methods.

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/JcksonMCD/golang-jwt/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func userRouter(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controller.getUsers())
+	incomingRoutes.GET("/user:user_id", controller.getUserById())
+
+}
