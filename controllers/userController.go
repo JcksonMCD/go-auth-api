5eb82167df370b55ab0180182dc49888802dfda8
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-08 14:48:57 +0100

    Feat: Logic for opening new user collection added to controller.

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,5 +1,14 @@
 package controllers
 
+import (
+	"github.com/JcksonMCD/golang-jwt/database"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var UserCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var validate = validator.New()
+
 func HashPassword() {
 
 }
