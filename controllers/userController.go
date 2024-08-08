ea02251880525dcdc7f6f362f7e6d379fb2438c0
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-08 15:42:06 +0100

    Feat: Logic added to sign up func to check against existing db and throw errors if the phone number or email is already associated with a different account.

--- controllers/userController.go
+++ controllers/userController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"time"
 
@@ -25,8 +26,55 @@ func VerifyPassword() {
 
 }
 
-func Signup() {
+// Signup returns a handler function that registers a new user.
+func Signup() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var user models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Validate the user data.
+		if err := validate.Struct(user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Check if the email already exists in the database.
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		if err != nil {
+			log.Println("Error occurred while checking email:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while checking email."})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusConflict, gin.H{"error": "This email is already being used by another user account."})
+			return
+		}
+
+		// Check if the phone number already exists in the database.
+		count, err = UserCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		if err != nil {
+			log.Println("Error occurred while checking phone number:", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while checking phone number."})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusConflict, gin.H{"error": "This phone number is already being used by another user account."})
+			return
+		}
+
+		// saving logic to go here....
+
+		c.JSON(http.StatusOK, gin.H{"message": "User registered successfully"})
+	}
 }
 
 func Login() {
