332f37d0c20ba4955b098f9f170c0e548512fdaa
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-08 14:16:13 +0100

    Feat: .env file configirations for port and url set up.

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -1,1 +1,18 @@
 package database
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func DBinstance() *mongo.Client {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file.")
+	}
+
+	MongoDB := os.Getenv("MONGODB_URL")
+}
