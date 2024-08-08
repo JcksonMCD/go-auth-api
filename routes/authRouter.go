eb9ad6605dafd73dd456334cee1bb77b0df170a6
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-08 13:13:53 +0100

    Fix: Capatalization errors for method names in routes file fixed.

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func authRouter(incomingRoutes *gin.Engine) {
+func AuthRouter(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("users/signup", controller.Signup())
 	incomingRoutes.POST("users/login", controller.Login())
 }
