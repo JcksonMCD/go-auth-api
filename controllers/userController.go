50176d7e3545cb98c9d0d1b4153e712e04a3926c
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-08 14:57:20 +0100

    Feat: get user id method built out to check if user type is admin.

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/JcksonMCD/golang-jwt/database"
+	helper "github.com/JcksonMCD/golang-jwt/service"
+	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -29,6 +33,14 @@ func GetUsers() {
 
 }
 
-func GetUserById() {
+func GetUserById() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userId := c.Param("user_id")
 
+		err := helper.MatchUserTypeToID(c, userId)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+	}
 }
