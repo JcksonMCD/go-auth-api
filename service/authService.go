cbe78fcf9c0c1548603aa3b9925394c58d96a219
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-08 15:05:53 +0100

    Feat: Func added auth service class which checks user type and id to see if they have access to the records they are trying to access. If they do not then throws an error.

--- service/authService.go
+++ service/authService.go
@@ -1,5 +1,21 @@
 package service
 
-func MatchUserTypeToID() {
+import (
+	"errors"
 
+	"github.com/gin-gonic/gin"
+)
+
+func MatchUserTypeToID(c *gin.Context, userId string) error {
+	userType := c.GetString("user_type")
+	uid := c.GetString("uid")
+	err := nil
+
+	if userType == "USER" && uid != userId {
+		err = errors.New("Unauthorised to access this resource")
+		return err
+	}
+	err = CheckUserType(c, userType)
+
+	return err
 }
