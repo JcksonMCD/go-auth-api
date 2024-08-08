1c764f4e0aee34a637505ede0d77970d6112356a
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-08 15:11:54 +0100

    Feat: Helper func created to check user role to see if resource access is allowed.

--- service/authService.go
+++ service/authService.go
@@ -6,16 +6,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// MatchUserTypeToID checks if the user type matches the user ID and verifies authorization.
 func MatchUserTypeToID(c *gin.Context, userId string) error {
 	userType := c.GetString("user_type")
 	uid := c.GetString("uid")
-	err := nil
 
+	// If user is a regular user and uid does not match the given userId, return an authorization error.
 	if userType == "USER" && uid != userId {
-		err = errors.New("Unauthorised to access this resource")
-		return err
+		return errors.New("Unauthorized to access this resource")
 	}
-	err = CheckUserType(c, userType)
 
-	return err
+	return CheckUserType(c, userType)
+}
+
+// CheckUserType verifies if the user has the required role to access the resource (ADMIN).
+func CheckUserType(c *gin.Context, role string) error {
+	userType := c.GetString("user_type")
+
+	if userType != role {
+		return errors.New("Unauthorized to access this resource")
+	}
+
+	return nil
 }
