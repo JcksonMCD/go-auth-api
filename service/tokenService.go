2d75cb5ca075ab4ebebeb81b61675a51fe57eb42
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-22 13:27:20 +0100

    Feat: Claims struct created with 24 hour expiry date.

--- service/tokenService.go
+++ service/tokenService.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"os"
+	"time"
 
 	"github.com/JcksonMCD/golang-jwt/database"
 	jwt "github.com/dgrijalva/jwt-go"
@@ -21,5 +22,14 @@ var UserCollection *mongo.Collection = database.OpenCollection(database.Client, 
 var SECRET_KEY = os.Getenv("SECRET_KEY")
 
 func GenerateAllTokens(email string, firstName string, lastName string, userType string, uid string) (signedToken string, signedRefreshToken string, err error) {
-
+	claims := &SignedDetails{
+		Email:      email,
+		First_Name: firstName,
+		Last_Name:  lastName,
+		Uid:        uid,
+		User_Type:  userType,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
 }
