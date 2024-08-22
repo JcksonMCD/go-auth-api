0f0372c8f5497724255f8279d988c811e720879d
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-22 16:27:15 +0100

    Feat: Update all tokens func complete now alters db documents to amend tokens based of user id.

--- service/tokenService.go
+++ service/tokenService.go
@@ -1,13 +1,16 @@
 package service
 
 import (
+	"context"
 	"log"
 	"os"
 	"time"
 
 	"github.com/JcksonMCD/golang-jwt/database"
 	jwt "github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type SignedDetails struct {
@@ -54,6 +57,38 @@ func GenerateAllTokens(email string, firstName string, lastName string, userType
 	return token, refreshToken, err
 }
 
-func UpdateAllTokens() {
+func UpdateAllTokens(signedToken string, signedRefreshToken string, userId string) {
+	//Create a context with a timeout to ensure the operation doesn't run indefinitely.
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
 
+	//prepare the fields to be updated in the database.
+	updateFields := bson.D{
+		{"token", signedToken},
+		{"refresh_token", signedRefreshToken},
+		{"updated_at", time.Now()},
+	}
+
+	// define the filter to locate the specific user in the database using id field.
+	filter := bson.M{"user_id": userId}
+
+	// Step 4: Set up the update options, specifically enabling upsert (create the document if it doesn't exist).
+	upsert := true
+	options := options.Update().SetUpsert(upsert) // Enable upsert to create a new document if one doesn't exist.
+
+	_, err := UserCollection.UpdateOne(
+		ctx, // Pass the context to ensure the operation respects the timeout.
+		filter,
+		bson.D{
+			{"$set", updateFields}, // Use the $set operator to update the specified fields.
+		},
+		options,
+	)
+
+	if err != nil {
+		log.Printf("Failed to update tokens for user %s: %v", userId, err)
+		return
+	}
+
+	return
 }
