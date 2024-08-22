a2fa88ed41c4add28d5bed5fe4788ee0d565d793
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-22 17:25:18 +0100

    Feat: Get Users func completed using mongo db aggregation pipeline. Firs checks if users are admin and handles request parameters using default values if none are found.

--- controllers/userController.go
+++ controllers/userController.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/JcksonMCD/golang-jwt/database"
@@ -164,8 +165,75 @@ func Login() gin.HandlerFunc {
 	}
 }
 
-func GetUsers() {
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// check if the user is an admin and therefor has access
+		if err := service.CheckUserType(c, "ADMIN"); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
+		// create context with timeout and close after use
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		// Handle pagination parameters from the request query
+		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
+		if err != nil || recordPerPage < 1 {
+			recordPerPage = 10 // default to 10 records per page if parameter is missing or invalid
+		}
+
+		page, err := strconv.Atoi(c.Query("page"))
+		if err != nil || page < 1 {
+			page = 1 // default to page 1 if parameter is missing or invalid
+		}
+
+		// calculate the starting index for pagination
+		startIndex := (page - 1) * recordPerPage
+
+		// MongoDB aggregation pipeline stages
+		// match stage to filter documents
+		matchStage := bson.D{{"$match", bson.D{}}}
+
+		// group stage to count total documents and group all user documents
+		groupStage := bson.D{{
+			"$group", bson.D{
+				{"_id", nil},
+				{"total_count", bson.D{{"$sum", 1}}},
+				{"data", bson.D{{"$push", "$$ROOT"}}},
+			},
+		}}
+
+		// project stage to reshape the output, including pagination
+		projectStage := bson.D{{
+			"$project", bson.D{
+				{"_id", 0},
+				{"total_count", 1},
+				{"user_items", bson.D{{"$slice", []interface{}{"$data", startIndex, recordPerPage}}}},
+			},
+		}}
+
+		// execute the aggregation pipeline
+		cursor, err := UserCollection.Aggregate(ctx, mongo.Pipeline{matchStage, groupStage, projectStage})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while listing users"})
+			return
+		}
+
+		// retrieve all results
+		var allUsers []bson.M
+		if err = cursor.All(ctx, &allUsers); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		// return the first element of the aggregated results
+		if len(allUsers) > 0 {
+			c.JSON(http.StatusOK, allUsers[0])
+		} else {
+			c.JSON(http.StatusOK, gin.H{"message": "no users found"})
+		}
+	}
 }
 
 func GetUserById() gin.HandlerFunc {
