6a12a101628c20cd3500ba0a70f9349b7bf69cfa
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-22 13:06:37 +0100

    Feat: Signed Details struct created in token service file.

--- service/tokenService.go
+++ service/tokenService.go
@@ -1,1 +1,14 @@
 package service
+
+import (
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+type SignedDetails struct {
+	Email      string
+	First_Name string
+	Last_Name  string
+	Uid        string
+	User_Type  string
+	jwt.StandardClaims
+}
