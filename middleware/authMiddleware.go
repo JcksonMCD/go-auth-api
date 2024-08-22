b1dcf9e4adec087ce2f3003ee31d8c140f2732b8
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-22 19:44:54 +0100

    Feat: Authenticate func complete. Context fields now set to return values from ValidateToken().

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -21,10 +21,19 @@ func Authenticate() gin.HandlerFunc {
 
 		// validate the token
 		claims, err := service.ValidateToken(clientToken)
-		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		if err != "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err})
 			c.Abort() // Abort the request processing
 			return
 		}
+
+		// set context fields to those that have been returned
+		c.Set("email", claims.Email)
+		c.Set("first_name", claims.First_Name)
+		c.Set("last_name", claims.Last_Name)
+		c.Set("uid", claims.Uid)
+		c.Set("user_type", claims.User_Type)
+		c.Next()
+
 	}
 }
