87ffe1e713263d3cd0359580bcb7521f6c1a1af7
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2024-08-22 18:02:23 +0100

    Feat: Authenticate func logic added for first steps- Get information from header and validate the token is provided.

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,9 +1,30 @@
 package middleware
 
 import (
+	"net/http"
+
+	service "github.com/JcksonMCD/golang-jwt/service"
 	"github.com/gin-gonic/gin"
 )
 
 func Authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// retrieve the token from the request header
+		clientToken := c.Request.Header.Get("token")
 
+		// check if the token is provided in the request header
+		if clientToken == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "no authorization header provided"})
+			c.Abort() // Abort the request processing
+			return
+		}
+
+		// validate the token
+		claims, err := service.ValidateToken(clientToken)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			c.Abort() // Abort the request processing
+			return
+		}
+	}
 }
